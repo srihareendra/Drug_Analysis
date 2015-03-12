85007b975109f4fab3e3473bbc2cc72c08aed9df
srihareendra <srihari.naruto@gmail.com>
2015-03-12 17:02:54 +0530

    Drug analysus

---
+++ README.md
@@ -0,0 +1,12 @@
+Rquirements and Steps to bee followed
+1.You require eclipse ide installed
+2.Fork the project.Import the project into eclipse work space(You need have android ADT plugin installed you can fin ti here http://developer.android.com/tools/sdk/eclipse-adt.html)
+3.Download and install android NDK Update the path in system variables and eclipse--.windows-->preferences-->android-->NDK
+4.Fork tess-two project from here https://github.com/rmtheis/tess-two
+5 Open CMD or shell go to the folder containing tess-two and type ndk-build
+5import the tess-two project into eclipse and change it to Library project(project-->properties-->android-->Is Library tick)
+6.Import the Drug analysis into your workspace(containing tess-two) and add tess-two as library( project-->properties-->android-->add-->add tess two)
+7.Run it .It works
+
+
+I have included open cv snippets as comments if you want further enchance the code you can uncomment them and for full details and explanation you can visit https://c3.csc.com/groups/innovation-labs/blog/2015/01/12/drug-recommendation-application-in-android-and-python-using-ocr  
---
+++ src/com/datumdroid/android/ocr/simple/SimpleAndroidOCRActivity.java
@@ -0,0 +1,299 @@
+package com.datumdroid.android.ocr.simple;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.http.client.ClientProtocolException;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+public class SimpleAndroidOCRActivity extends Activity implements AsyncResponse {
+public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
+public static final String DATA_PATH = Environment
+.getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
+//private CameraBridgeViewBase mOpenCvCameraView;
+// You should have the trained data file in assets folder
+// You can get them at:
+// http://code.google.com/p/tesseract-ocr/downloads/list
+public static final String lang = "eng";
+private static final String TAG = "SimpleAndroidOCR.java";
+private static final String TAG_1 = "SimpleAndroidOCRActivity::Activity";
+protected Button _button;
+// protected ImageView _image;
+protected EditText _field;
+protected String _path;
+protected boolean _taken;
+String drug_string;
+protected static final String PHOTO_TAKEN = "photo_taken";
+/*private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+@Override
+public void onManagerConnected(int status) {
+switch (status) {
+case LoaderCallbackInterface.SUCCESS:
+{
+Log.i(TAG_1, "OpenCV hah hah loaded successfully");
+} break;
+default:
+{
+super.onManagerConnected(status);
+} break;
+}
+}
+};
+@Override
+public void onResume()
+{
+super.onResume();
+OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+}*/
+@Override
+public void onCreate(Bundle savedInstanceState) {
+String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+for (String path : paths) {
+File dir = new File(path);
+if (!dir.exists()) {
+if (!dir.mkdirs()) {
+Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+return;
+} else {
+Log.v(TAG, "Created directory " + path + " on sdcard");
+}
+}
+}
+// lang.traineddata file with the app (in assets folder)
+// You can get them at:
+// http://code.google.com/p/tesseract-ocr/downloads/list
+// This area needs work and optimization
+if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+try {
+AssetManager assetManager = getAssets();
+InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+//GZIPInputStream gin = new GZIPInputStream(in);
+OutputStream out = new FileOutputStream(DATA_PATH
++ "tessdata/" + lang + ".traineddata");
+// Transfer bytes from in to out
+byte[] buf = new byte[1024];
+int len;
+//while ((lenf = gin.read(buff)) > 0) {
+while ((len = in.read(buf)) > 0) {
+out.write(buf, 0, len);
+}
+in.close();
+//gin.close();
+out.close();
+Log.v(TAG, "Copied " + lang + " traineddata");
+} catch (IOException e) {
+Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+}
+}
+super.onCreate(savedInstanceState);
+setContentView(R.layout.main);
+// _image = (ImageView) findViewById(R.id.image);
+_field = (EditText) findViewById(R.id.field);
+_button = (Button) findViewById(R.id.button);
+_button.setOnClickListener(new ButtonClickHandler());
+_path = DATA_PATH + "/ocr.jpg";
+}
+public class ButtonClickHandler implements View.OnClickListener {
+public void onClick(View view) {
+Log.v(TAG, "Starting Camera app");
+startCameraActivity();
+}
+}
+// Simple android photo capture:
+// http://labs.makemachine.net/2010/03/simple-android-photo-capture/
+protected void startCameraActivity() {
+File file = new File(_path);
+Uri outputFileUri = Uri.fromFile(file);
+final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+startActivityForResult(intent, 0);
+}
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+Log.i(TAG, "resultCode: " + resultCode);
+if (resultCode == -1) {
+try {
+onPhotoTaken();
+} catch (ClientProtocolException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+} catch (IOException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+} else {
+Log.v(TAG, "User cancelled");
+}
+}
+@Override
+protected void onSaveInstanceState(Bundle outState) {
+outState.putBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN, _taken);
+}
+@Override
+protected void onRestoreInstanceState(Bundle savedInstanceState) {
+Log.i(TAG, "onRestoreInstanceState()");
+if (savedInstanceState.getBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN)) {
+try {
+onPhotoTaken();
+} catch (ClientProtocolException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+} catch (IOException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+}
+}
+public static Bitmap grayScaleImage(Bitmap src) {
+// constant factors
+final double GS_RED = 0.299;
+final double GS_GREEN = 0.587;
+final double GS_BLUE = 0.114;
+// create output bitmap
+Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+// pixel information
+int A, R, G, B;
+int pixel;
+// get image size
+int width = src.getWidth();
+int height = src.getHeight();
+// scan through every single pixel
+for(int x = 0; x < width; ++x) {
+for(int y = 0; y < height; ++y) {
+// get one pixel color
+pixel = src.getPixel(x, y);
+// retrieve color of all channels
+A = Color.alpha(pixel);
+R = Color.red(pixel);
+G = Color.green(pixel);
+B = Color.blue(pixel);
+// take conversion up to one single value
+R = G = B = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
+// set new pixel color to output bitmap
+bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+}
+}
+// return final image
+return bmOut;
+}
+protected void onPhotoTaken() throws ClientProtocolException, IOException {
+_taken = true;
+BitmapFactory.Options options = new BitmapFactory.Options();
+options.inSampleSize = 6;
+Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+TextView mainTextView = (TextView)findViewById(R.id.Output);
+mainTextView.setText("Fetching Drug Details.........");
+try {
+ExifInterface exif = new ExifInterface(_path);
+int exifOrientation = exif.getAttributeInt(
+ExifInterface.TAG_ORIENTATION,
+ExifInterface.ORIENTATION_NORMAL);
+Log.v(TAG, "Orient: " + exifOrientation);
+int rotate = 0;
+switch (exifOrientation) {
+case ExifInterface.ORIENTATION_ROTATE_90:
+rotate = 90;
+break;
+case ExifInterface.ORIENTATION_ROTATE_180:
+rotate = 180;
+break;
+case ExifInterface.ORIENTATION_ROTATE_270:
+rotate = 270;
+break;
+}
+Log.v(TAG, "Rotation: " + rotate);
+if (rotate != 0) {
+// Getting width & height of the given image.
+int w = bitmap.getWidth();
+int h = bitmap.getHeight();
+// Setting pre rotate
+Matrix mtx = new Matrix();
+mtx.preRotate(rotate);
+// Rotating Bitmap
+bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+}
+// Convert to ARGB_8888, required by tess
+bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+} catch (IOException e) {
+Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+}
+// _image.setImageBitmap( bitmap );
+Log.v(TAG, "Before baseApi");
+TessBaseAPI baseApi = new TessBaseAPI();
+baseApi.setDebug(true);
+baseApi.init(DATA_PATH, lang);
+/*Mat tmp = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+Utils.bitmapToMat(bitmap, tmp);
+Mat gray = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+Mat mIntermediateMat = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+Mat mRgba = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+Imgproc.cvtColor(tmp, gray, Imgproc.COLOR_RGB2GRAY);
+Imgproc.Canny(gray, mIntermediateMat, 35, 75);
+Imgproc.GaussianBlur(gray, gray, new Size(5, 5), 2, 2);
+Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2BGRA, 4);
+Utils.matToBitmap(gray, bitmap);*/
+bitmap=grayScaleImage(bitmap);
+baseApi.setImage(bitmap);
+String recognizedText = baseApi.getUTF8Text();
+baseApi.end();
+// You now have the text in recognizedText var, you can do anything with it.
+// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+// so that garbage doesn't make it to the display.
+Log.v(TAG, "OCRED TEXT: " + recognizedText);
+if ( lang.equalsIgnoreCase("eng") ) {
+recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+}
+recognizedText = recognizedText.trim();
+if ( recognizedText.length() != 0 ) {
+_field.setText(_field.getText().toString().length() == 0 ? recognizedText : recognizedText);
+_field.setSelection(_field.getText().toString().length());
+Log.v(TAG, "_field TEXT:" + _field.getText());
+drug_string= _field.getText().toString().replaceAll(" ", " ");
+drug_string.toLowerCase();
+String strOrig = drug_string;
+
+int intIndex = strOrig.indexOf("fepan");
+if(intIndex == - 1){
+	 drug_string= _field.getText().toString().replaceAll(" ", "\n");
+}else{
+  
+ drug_string="fepanil";
+}
+
+
+//String drug_string= _field.getText().toString();
+//String[] spplit=drug_string.split("\\s+");
+Log.v(TAG, "_field TEXT:" + drug_string);
+}
+String url = "https://drugpedia-stage.herokuapp.com/drug_service/api/v1/peek" ;
+String output;
+MyAsyncTask myAsyncTask = new MyAsyncTask(this);
+myAsyncTask.delegate = this;
+myAsyncTask.execute(url, drug_string);
+}
+public void processFinish(String output){
+TextView mainTextView = (TextView)findViewById(R.id.Output);
+//in your OnCreate() method
+mainTextView.setText(output);
+}
+}
